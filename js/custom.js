f5324e47929ccd0cce1fdf9cd4c70ba723e16b8b
Jio Im <148739315+JioIm@users.noreply.github.com>
2023-12-21 18:14:12 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,26 @@
 const MAIN_SLIDE = new Swiper('.main_visual_slide', {
-    slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
+
+$(function () {
+  $('.event_tab li').on('click', function (e) {
+    e.preventDefault();
+    const idx = $(this).index();
+    $('.tab_content .content')
+      .eq(idx)
+      .addClass('on')
+      .siblings()
+      .removeClass('on');
+    $(this)
+      .addClass('on')
+      .siblings()
+      .removeClass('on');
+  });
+
 })
