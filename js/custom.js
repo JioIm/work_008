507f3d51f7e07733fd6c22a70ee30a05864c108f
Jio Im <148739315+JioIm@users.noreply.github.com>
2023-12-27 01:31:26 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,51 @@
-const MAIN_SLIDE = new Swiper('.main_visual_slide', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-})
+
+// const MAIN_VISUAL_SLIDE = new Swiper('.main_visual_slide', {
+//   loop: true,
+//   parallax: true,
+//   speed: 1000,
+
+//   slideActiveClass: 'on',
+// });
+
+// const MAIN_VS_LEFT_BUTTON = document.querySelector('.MainVisual .main_slide_arrows .left');
+// MAIN_VS_LEFT_BUTTON.addEventListener('click', () => {
+//   MAIN_VISUAL_SLIDE.slidePrev();
+// });
+
+// const MAIN_VS_RIGHT_BUTTON = document.querySelector('.MainVisual .main_slide_arrows .right');
+// MAIN_VS_RIGHT_BUTTON.addEventListener('click', () => {
+//   MAIN_VISUAL_SLIDE.slideNext();
+// });
+
 
 $(function () {
-  $('.event_tab li').on('click', function (e) {
-    e.preventDefault();
-    const idx = $(this).index();
-    $('.tab_content .content')
+  $('.MainVisual .main_visual_slide').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    arrows: false,
+});
+
+$('.MainVisual .main_slide_arrows .left').on('click', function () {
+        $('.main_visual_slide').slick('slickPrev')
+});
+
+$('.MainVisual .main_slide_arrows .right').on('click', function () {
+        $('.main_visual_slide').slick('slickNext')
+});
+
+$('.main_tab li').on('click', function (e) {
+  e.preventDefault();
+  const idx = $(this).index();
+  $('.tab_content .content')
       .eq(idx)
       .addClass('on')
       .siblings()
       .removeClass('on');
-    $(this)
+  $(this)
       .addClass('on')
       .siblings()
       .removeClass('on');
-  });
+});
 
 })
